7b9f8bf4e43c44b87a5338a98de4932315ff9c23
Lee Mark Guiruela <asianmasseur06@gmail.com>
2022-04-10 13:48:31 -0400

    commit tree 197470ef7bdc5f32ef15af4cf23df19b5f8a2633

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+const employee = {
+    name: "Leemark Guiruela", 
+    streetAddress: "110 Madison Ave",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return({...employee,[key]: value,})
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+employee[key] = value 
+return employee
+}
